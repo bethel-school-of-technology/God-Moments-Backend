e05a48b2567f84b0acd9d13a10269a9534c08f56
Javier Martinez <50782437+JaviMars91@users.noreply.github.com>
2020-06-08 02:55:08 -0700

    message controller/ repository & DB updates

---
+++ God_Moments_Backend/src/main/java/com/godmoments/chat/Message.java
@@ -0,0 +1,38 @@
+package com.godmoments.chat;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="message")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ God_Moments_Backend/src/main/java/com/godmoments/chat/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.godmoments.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+}
