ac3612ca76fdba26e5931d00678619ebed82c8b0
Javier Martinez <50782437+JaviMars91@users.noreply.github.com>
2020-06-17 03:56:14 -0700

    email and password in Users

--- GMBE/src/main/java/com/gmbe/demo/models/Users.java
+++ GMBE/src/main/java/com/gmbe/demo/models/Users.java
@@ -14,6 +14,8 @@ public class Users {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private String email;
+	private String password;
 	private String content;
 
 	public Integer getId() {
@@ -31,6 +33,24 @@ public class Users {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+			
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	
+	}	
 
 	public String getContent() {
 		return content;
